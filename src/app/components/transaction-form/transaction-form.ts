b11dfd3eef574902267cfef4358752f52fb762f7
Dumindu Dissanayake <duminduad@icloud.com>
2025-07-21 00:15:26 +0530

    feat: implement transaction form with styling and category management

--- src/app/components/transaction-form/transaction-form.ts
+++ src/app/components/transaction-form/transaction-form.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Transaction } from '../../models/transaction';
 
 @Component({
   selector: 'app-transaction-form',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './transaction-form.html',
-  styleUrl: './transaction-form.css'
+  styleUrl: './transaction-form.css',
 })
-export class TransactionForm {
+export class TransactionForm implements OnInit {
+  newTransaction: Transaction = {
+    id: 0,
+    type: '',
+    category: '',
+    amount: 0,
+    createAt: new Date(),
+    updatedAt: new Date(),
+  };
 
+  incomeCategories = ['Salary', 'Investment', 'Freelance'];
+  expenseCategories = [
+    'Food',
+    'Transport',
+    'Utilities',
+    'Entertainment',
+    'Health',
+  ];
+
+  availableCategories: string[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.updateCategories();
+  }
+
+  updateCategories(): void {
+    this.availableCategories =
+      this.newTransaction.type === 'Expense'
+        ? this.expenseCategories
+        : this.incomeCategories;
+    this.newTransaction.category = '';
+  }
+
+  addTransaction(): void {
+    console.log('Adding transaction:', this.newTransaction);
+    // TODO: Call the transaction service to save the transaction
+  }
 }
