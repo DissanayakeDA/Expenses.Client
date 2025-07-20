e4159424df775bf32067a9716d0dffe3fd423ed0
Dumindu Dissanayake <duminduad@icloud.com>
2025-07-20 09:44:31 +0530

    feat: add authentication and transaction management components
    - Implemented routing for login, signup, transaction list, and transaction form.
    - Created Footer and Header components with basic templates.
    - Developed Login and Signup components with placeholder templates.
    - Added TransactionList and TransactionForm components for managing transactions.
    - Defined Transaction model interface for type safety.
    - Built Transaction service for API interactions with CRUD operations.
    - Added unit tests for all new components and services.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './components/footer/footer';
+import { Header } from './components/header/header';
+import { TransactionList } from './components/transaction-list/transaction-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Expenses.Client');
---
+++ src/app/services/transaction.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Transaction {
+  private apiUrl = 'https://localhost:7170/api/Transactions';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Transaction[]> {
+    return this.http.get<Transaction[]>(this.apiUrl + '/All');
+  }
+  getById(id: number): Observable<Transaction[]> {
+    return this.http.get<Transaction[]>(this.apiUrl + '/Details' + id);
+  }
+
+  create(transaction: Transaction): Observable<Transaction[]> {
+    return this.http.post<Transaction[]>(this.apiUrl + '/Create', transaction);
+  }
+  update(id: number, transaction: Transaction): Observable<Transaction[]> {
+    return this.http.post<Transaction[]>(
+      this.apiUrl + '/Update' + id,
+      transaction
+    );
+  }
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(this.apiUrl + '/Delete' + id);
+  }
+}
