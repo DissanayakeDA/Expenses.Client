c3db0ea5bee82131706f52489fed35040be6116f
Dumindu Dissanayake <duminduad@icloud.com>
2025-07-20 12:57:41 +0530

    feat: implement main application structure and enhance footer and header components

--- src/app/components/transaction-list/transaction-list.ts
+++ src/app/components/transaction-list/transaction-list.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Transaction } from '../../models/transaction';
 
 @Component({
   selector: 'app-transaction-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './transaction-list.html',
-  styleUrl: './transaction-list.css'
+  styleUrl: './transaction-list.css',
 })
 export class TransactionList {
-
+  transactions: Transaction[] = [
+    {
+      id: 1,
+      type: 'Income',
+      category: 'Salary',
+      amount: 5000,
+      createdAt: new Date('2023-01-01'),
+      updatedAt: new Date('2023-01-01'),
+    },
+    {
+      id: 2,
+      type: 'Expense',
+      category: 'Groceries',
+      amount: 200,
+      createdAt: new Date('2023-01-02'),
+      updatedAt: new Date('2023-01-02'),
+    },
+    // Add more transactions as needed
+  ];
 }
